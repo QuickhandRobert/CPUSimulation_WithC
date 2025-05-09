5f9d5a0d6f2a545b4b650400dc5a71e5ae068e31
QuickhandRobert <farbod.keshavarz84@gmail.com>
2025-05-09 10:26:31 +0330

    Logic Operations Implemented

---
+++ Operations.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "systemConfiguration.h"
+
+//Function Declration
+void OR(int, int, int);
+void AND(int, int, int);
+void NOT(int);
+void XOR(int, int, int);
+
+extern int systemMemory[MEMORY_SIZE];
+extern int userMemory[MEMORY_SIZE][STRING_SIZE];
+
+void OR(int m1, int m2, int m3){
+	userMemory[m3][0] = userMemory[m1][0] || userMemory[m2][0];
+} 
+void AND(int m1, int m2, int m3){
+	userMemory[m3][0] = userMemory[m1][0] && userMemory[m2][0];
+} 
+void NOT(int m1){
+	userMemory[m1][0] = !userMemory[m1][0];
+} 
+void XOR(int m1, int m2, int m3){
+	userMemory[m3][0] = userMemory[m1][0] ^ userMemory[m2][0];
+} 
+
+
---
+++ fileManagement.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#include "systemConfiguration.h"
+
+extern int systemMemory[MEMORY_SIZE];
+extern int userMemory[MEMORY_SIZE][STRING_SIZE];
