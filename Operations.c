a0849d4f4c6d073ade6184770f356f1237fadc19
PurePurpleDiva <divauta4@gmail.com>
2025-05-16 12:11:49 +0430

    Arithmetic Implemented

--- Operations.c
+++ Operations.c
@@ -6,6 +6,14 @@ void OR(int, int, int);
 void AND(int, int, int);
 void NOT(int);
 void XOR(int, int, int);
+void ADD(int, int, int);
+void SUB(int, int, int);
+void DIV(int, int, int);
+void MULT(int, int, int);
+void LO(int, int, int);
+void INC(int);
+void DEC(int);
+void NEG(int);
 
 extern int systemMemory[MEMORY_SIZE];
 extern int userMemory[MEMORY_SIZE][STRING_SIZE];
@@ -22,5 +30,30 @@ void NOT(int m1){
 void XOR(int m1, int m2, int m3){
 	userMemory[m3][0] = userMemory[m1][0] ^ userMemory[m2][0];
 } 
-
+void ADD(int m1, int m2, int m3){
+	userMemory[m3][0]=userMemory[m1][0]+userMemory[m2][0];
+}
+void SUB(int m1, int m2, int m3){
+	userMemory[m3][0]=userMemory[m1][0]-userMemory[m2][0];
+}
+void DIV(int m1, int m2, int m3){
+	if(userMemory[m2][0]!= 0){
+		userMemory[m3][0]=userMemory[m1][0]/userMemory[m2][0];
+	}
+}
+void MULT(int m1, int m2, int m3){
+	userMemory[m3][0]=userMemory[m1][0]*userMemory[m2][0];
+}
+void LO(int m1 , int m2 , int m3){
+	userMemory[m3][0] = userMemory[m1][0] % userMemory[m2][0];
+}
+void INC(int m1){
+	userMemory[m1][0] = userMemory[m1][0]++;
+}
+void DEC(int m1){
+	userMemory[m1][0] = userMemory[m1][0]--;
+}
+void NEG(int m1){
+	userMemory[m1][0] = -userMemory[m1][0];
+}
 
--- systemConfiguration.h
+++ systemConfiguration.h
@@ -1,3 +1,26 @@
 #define MEMORY_SIZE 256
 #define STRING_SIZE 256
 #define EMPTY_MEMORY_VALUE 0x7FFFFFFF
+#define ADD 1
+#define SUB 2
+#define MUL 3
+#define DIV 4
+#define LO 5
+#define INC 6
+#define DEC 7
+#define NEG 8
+#define SET 10
+#define COPY 11
+#define FLUSH 12
+#define CLEAR 13
+#define CMP 14
+#define BCMP 15
+#define EQ 16
+#define OR 17
+#define AND 18
+#define NOT 19
+#define XOR 20
+#define XNOT 21
+#define NAND 22
+#define NOR 23
+#define 
